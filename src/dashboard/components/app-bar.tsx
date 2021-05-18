c462453218ea5ee40530a841c56107db1f45b827
Calvin Ling <calvinling8@gmail.com>
2021-05-17 21:59:21 -0400

    Add link to home

--- src/dashboard/components/app-bar.tsx
+++ src/dashboard/components/app-bar.tsx
@@ -1,10 +1,26 @@
-import { AppBar as MuiAppBar, Toolbar, Typography } from '@material-ui/core';
+import {
+  AppBar as MuiAppBar,
+  Button,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import paths from '../../common/constants/paths';
+import useStyles from './app-bar.styles';
 
 export default function AppBar() {
+  const classes = useStyles();
   return (
     <MuiAppBar position="sticky">
       <Toolbar>
-        <Typography variant="h6">Basic Flashcards</Typography>
+        <Button
+          color="inherit"
+          component={Link}
+          to={paths.home}
+          className={classes.homeLink}
+        >
+          <Typography variant="h6">Basic Flashcards</Typography>
+        </Button>
       </Toolbar>
     </MuiAppBar>
   );
