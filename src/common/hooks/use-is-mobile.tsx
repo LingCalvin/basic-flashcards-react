4c0001d903cc0a5c040be1c7dda78cf309b8fffd
Calvin Ling <calvinling8@gmail.com>
2021-05-24 13:53:59 -0400

    Fix search bar flashing on small screens
    Set the noSsr option in useMediaQuery(). This stops useMediaQuery() from
    causing two renders with an initial value of false.

--- src/common/hooks/use-is-mobile.tsx
+++ src/common/hooks/use-is-mobile.tsx
@@ -2,6 +2,6 @@ import { useMediaQuery, useTheme } from '@material-ui/core';
 
 export default function useIsMobile() {
   const theme = useTheme();
-  const match = useMediaQuery(theme.breakpoints.down('xs'));
+  const match = useMediaQuery(theme.breakpoints.down('xs'), { noSsr: true });
   return match;
 }
